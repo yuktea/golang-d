746637d77f23d235996abcca24624e61855cda0c
yuktea <73362292+yuktea@users.noreply.github.com>
2024-02-11 23:30:06 +0530

    test: add test for non post request method

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -25,3 +25,19 @@ func TestHandleCommandNotFound(t *testing.T) {
         t.Errorf("handler returned wrong status code for a command not found: got %v want %v", status, http.StatusNotFound)
     }
 }
+
+func TestHandleCommandNonPostMethod(t *testing.T) {
+    req, err := http.NewRequest(http.MethodGet, "/api/cmd", nil)
+    if err != nil {
+        t.Fatalf("Could not create request: %v", err)
+    }
+
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(HandleCommand)
+
+    handler.ServeHTTP(rr, req)
+
+    if status := rr.Code; status != http.StatusMethodNotAllowed {
+        t.Errorf("handler returned wrong status code for non-POST method: got %v want %v", status, http.StatusMethodNotAllowed)
+    }
+}
